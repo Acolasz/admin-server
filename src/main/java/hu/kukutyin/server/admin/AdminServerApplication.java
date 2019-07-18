da269f2163816bfb8a175943148c5cad7ff4e039
Olasz <ocsko.oliver13@gmail.com>
2019-07-18 21:36:22 +0200

    initial commit

---
+++ src/main/java/hu/kukutyin/server/admin/AdminServerApplication.java
@@ -0,0 +1,15 @@
+package hu.kukutyin.server.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+}
