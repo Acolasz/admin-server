e507bf295c19c8a45b2b09719f1461118877c9d1
Olasz <ocsko.oliver13@gmail.com>
2019-08-01 23:07:52 +0200

    working connection with client app (first-cucc) https://www.javadevjournal.com/spring-boot/spring-boot-admin/

--- src/main/java/hu/kukutyin/server/admin/AdminServerApplication.java
+++ src/main/java/hu/kukutyin/server/admin/AdminServerApplication.java
@@ -1,15 +1,37 @@
 package hu.kukutyin.server.admin;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableAdminServer
 public class AdminServerApplication {
 
+	private final String adminContextPath;
+
+	public AdminServerApplication(AdminServerProperties adminServerProperties) {
+		this.adminContextPath = adminServerProperties.getContextPath();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(AdminServerApplication.class, args);
 	}
 
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChainSecure(ServerHttpSecurity http) {
+
+		return http.authorizeExchange()
+				.pathMatchers(adminContextPath + "/assets/**").permitAll()
+				.pathMatchers(adminContextPath + "/login").permitAll()
+				.anyExchange().authenticated()
+				.and().formLogin().loginPage(adminContextPath + "/login")
+				.and().logout().logoutUrl(adminContextPath + "/logout")
+				.and().httpBasic()
+				.and().csrf().disable().build();
+	}
 }
